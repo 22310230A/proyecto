8fef12a813643322df0e403d7fd25fc33820b159
22310230A <143145351+22310230A@users.noreply.github.com>
2023-10-29 05:25:34 +0000

    Avance hasta 28/10/23

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# proyecto
+# Proyecto
+
+## Trabajo
+
+## Descripción
+
+### Escritura
+
+![](https://assets.misjuegos.com/9/12529/65493/672x448/fireboy-and-watergirl-the-forest-temple.webp)
+
+![](/out/docs//Diagrama_Firewater/Diagrama_Firewater.png)
---
+++ include/Firewater.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <Dibujo.hpp>
+#include <Actualizable.hpp>
+
+class Firewater : public Dibujo, public Actualizable
+{
+private:
+    /* data */
+public:
+    Firewater(/* args */) : Dibujo("fireboy")
+        {
+
+        }
+    ~Firewater
+
+() {}
+    void Actualizar() {
+        
+    }
+};
